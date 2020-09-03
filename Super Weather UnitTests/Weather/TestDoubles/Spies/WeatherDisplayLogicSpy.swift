1929b89c044ac67492b0403d7004f4e51255bfa9
Igor Silaev <cokturqi55@mail.ru>
2020-09-03 14:46:43 +0300

    Add presenter test

--- Super Weather UnitTests/Weather/TestDoubles/Spies/WeatherDisplayLogicSpy.swift
+++ Super Weather UnitTests/Weather/TestDoubles/Spies/WeatherDisplayLogicSpy.swift
@@ -12,31 +12,37 @@ import Foundation
 final class WeatherDisplayLogicSpy: WeatherModuleDisplayLogic {
     
     // MARK: - Private Properties
+    private(set) var isCalledDisplayActivityIndicator = false
+    private(set) var isCalledDisplayError = false
+    private(set) var isCalledDisplayWeather = false
+    private(set) var isCalledDisplayCitySearch = false
+    private(set) var isCalledDisplayInitialData = false
+    private(set) var isCalledDisplayWithoutDeleted = false
     
     // MARK: - Public Methods
     
     func displayActivityIndicator(isActive: Bool) {
-        
+        isCalledDisplayActivityIndicator = true
     }
     
     func displayError(_ errorModel: WeatherModels.Error.ErrorModel) {
-        
+        isCalledDisplayError = true
     }
     
     func displayWeather(_ viewModel: WeatherModels.Show.ViewModel) {
-        
+        isCalledDisplayWeather = true
     }
     
     func displayCitySearch() {
-        
+        isCalledDisplayCitySearch = true
     }
     
     func displayInitialData(_ viewModel: WeatherModels.FetchInitialData.ViewModel) {
-        
+        isCalledDisplayInitialData = true
     }
     
     func displayWithoutDeleted(_ viewModel: WeatherModels.Delete.ViewModel) {
-        
+        isCalledDisplayWithoutDeleted = true
     }
     
 }
--- Super Weather UnitTests/Weather/WeatherPresenterTests.swift
+++ Super Weather UnitTests/Weather/WeatherPresenterTests.swift
@@ -7,14 +7,17 @@
 //
 
 import XCTest
+import CoreData
 @testable import Super_Weather
+
+
 
 final class WeatherPresenterTests: XCTestCase {
     
     // MARK: - Private Properties
     
     private var sut: WeatherModulePresenter!
-    private var viewController: WeatherDisplayLogicSpy!
+    private var view: WeatherDisplayLogicSpy!
     
     // MARK: - Lifecycle
     
@@ -27,16 +30,55 @@ final class WeatherPresenterTests: XCTestCase {
         presenter.view = viewController
         
         sut = presenter
-        self.viewController = viewController
+        self.view = viewController
     }
     
     override func tearDown() {
         sut = nil
-        viewController = nil
+        view = nil
         
         super.tearDown()
     }
     
     // MARK: - Public Methods
-
+    
+    func testPresentLoading() {
+        sut.presentLoading(isActive: true)
+        
+        XCTAssertTrue(view.isCalledDisplayActivityIndicator, "Not started view.displayActivityIndicator")
+    }
+    
+    func testPresentWeatherWithError() {
+        
+        let response = WeatherModels.Fetch.Response(weather: nil, icon: nil, errorMessage: nil)
+        
+        sut.presentWeather(response)
+        
+        XCTAssertTrue(view.isCalledDisplayError, "Not started view.displayError")
+    }
+    
+    func testPresentWeather() {
+        let weather = WeatherAPI(coord: Coord(lon: 0, lat: 0), weather: [Weather(id: 0, main: "", weatherDescription: "", icon: "")], base: "", main: Main(temp: 0, pressure: 0, humidity: 0, tempMin: 0, tempMax: 0), visibility: 0, wind: Wind(speed: 0, deg: 0), clouds: Clouds(all: 0), dt: 0, sys: Sys(type: 0, id: 0, message: 0, country: "", sunrise: 0, sunset: 0), id: 0, name: "", cod: 0)
+        
+        let response = WeatherModels.Fetch.Response(weather: weather, icon: nil, errorMessage: nil)
+        
+        sut.presentWeather(response)
+        
+        XCTAssertTrue(view.isCalledDisplayWeather, "Not started view.displayWeather")
+    }
+    
+    func testPresentCitySearch() {
+        sut.presentCitySearch()
+        
+        XCTAssertTrue(view.isCalledDisplayCitySearch, "Not started view.displayCitySearch")
+    }
+    
+    func testPresentWithoutDeletedCity() {
+        let response = WeatherModels.Delete.Response(city: "London", index: 0)
+        
+        sut.presentWithoutDeletedCity(response)
+        
+        XCTAssertTrue(view.isCalledDisplayWithoutDeleted, "Not started view.displayWithoutDeleted")
+    }
 }
+
